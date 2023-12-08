326f94eefc95dc2d50df1c474480afd423f45e6a
Emanuel Frazão <emanuel.j.frazao@gmail.com>
2023-12-08 16:15:40 +0100

    file name

--- main.py
+++ main.py
@@ -31,40 +31,42 @@ if uploaded_file is not None:
     format = uploaded_file.name.split(".")[-1]
     st.audio(uploaded_file, format=f"audio/{format}", start_time=0)
 
-    # 2. Convert the wav to midi format
-    with st.spinner('Converting your chords to MIDI...'):
-        midi = convert_to_midi(uploaded_file)
-        instrument = midi.instruments[0]
+    if st.button("Generate"):
 
-        
-    with st.spinner('Generating your melody...'):
-    # 3. Preprocess the midi file for modelling
-        instrument_df = instrument_to_dataframe(instrument)
-        processed_df = process_rhythm_dataframe(instrument_df)
-        x_num, x_cat = preprocess_rhythm_subsequence(processed_df)
+        # 2. Convert the wav to midi format
+        with st.spinner('Converting your chords to MIDI...'):
+            midi = convert_to_midi(uploaded_file)
+            instrument = midi.instruments[0]
 
-        X_num = np.expand_dims(x_num, axis=0)
-        X_cat = np.expand_dims(x_cat, axis=0)
+            
+        with st.spinner('Generating your melody...'):
+        # 3. Preprocess the midi file for modelling
+            instrument_df = instrument_to_dataframe(instrument)
+            processed_df = process_rhythm_dataframe(instrument_df)
+            x_num, x_cat = preprocess_rhythm_subsequence(processed_df)
 
-    # 4. Get melody generation from model
-        melody = generate(x_num, x_cat, temperature=0.8)
-        melody_df = postprocess_melody_encoded(melody)
-        melody_instrument = dataframe_to_instrument(melody_df)
-        st.success("Melody generated!")
+            X_num = np.expand_dims(x_num, axis=0)
+            X_cat = np.expand_dims(x_cat, axis=0)
 
-    # 5. Join model generation with original MIDI track
-    with st.spinner('Putting together your chords and melody...'):
-        midi.instruments.append(melody_instrument)
-        st.success("Melody joined!")
+        # 4. Get melody generation from model
+            melody = generate(x_num, x_cat, temperature=0.8)
+            melody_df = postprocess_melody_encoded(melody)
+            melody_instrument = dataframe_to_instrument(melody_df)
+            st.success("Melody generated!")
 
-    # 6. Let the user download the MIDI file
-    midi_data = BytesIO()
-    midi.write(midi_data)
-    midi_data.seek(0)  # Reset the file pointer to the beginning
+        # 5. Join model generation with original MIDI track
+            midi.instruments.append(melody_instrument)
+            st.success("Melody joined!")
 
-    st.download_button(
-        label="Download your masterpiece!",
-        data=midi_data,
-        file_name="chords_with_melody.mid",
-        mime="audio/mid"
-    )
+        # 6. Let the user download the MIDI file
+        midi_data = BytesIO()
+        midi.write(midi_data)
+        midi_data.seek(0)  # Reset the file pointer to the beginning
+
+        file_name = st.text_input("Name your masterpiece", value="chords_with_melody.mid")
+        st.download_button(
+            label="Download your masterpiece!",
+            data=midi_data,
+            file_name=file_name,
+            mime="audio/mid"
+        )
